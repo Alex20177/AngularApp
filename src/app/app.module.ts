c54590c060c74a4f4b39b29c585f33ed2334cf4f
Alejandro Lopez Morales <alejandrolopez207@gmail.com>
2019-11-01 22:28:43 -0600

    First version, bootstrap grid layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './modules/card/card.component';
+import { MainComponent } from './modules/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
