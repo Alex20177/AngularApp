0ef4a3df0ed0249d91cf30172118834053ce220c
Alejandro Lopez Morales <alejandrolopez207@gmail.com>
2019-11-05 20:02:02 -0600

    This commit add a navbar for the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './modules/card/card.component';
 import { MainComponent } from './modules/main/main.component';
+import { MyNavBarComponent } from './modules/my-nav-bar/my-nav-bar.component';
+import { MyFooterComponent } from './modules/my-footer/my-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CardComponent,
-    MainComponent
+    MainComponent,
+    MyNavBarComponent,
+    MyFooterComponent
   ],
   imports: [
     BrowserModule,
